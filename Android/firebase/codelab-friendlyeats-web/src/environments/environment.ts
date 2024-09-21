f451a46ba673c9ea9aaa6e8c2d2d8a8361104868
Hari Thapliyaal <hari@dasarpai.com>
2024-09-21 21:59:58 +0530

    first commit

---
+++ Android/firebase/codelab-friendlyeats-web/src/environments/environment.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCAUmtvqd5q6iE_jMk5jOm4DBRsKRNR0cM",
+  authDomain: "friendlychat-5750a.firebaseapp.com",
+  projectId: "friendlychat-5750a",
+  storageBucket: "friendlychat-5750a.appspot.com",
+  messagingSenderId: "935355374293",
+  appId: "1:935355374293:web:31ab68c56ca237f01be05c",
+  measurementId: "G-FG834JE22N"
+};
---
+++ nodejs/node_passport_login/config/keys.js
@@ -0,0 +1,6 @@
+//dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dfPassword = 'mongodb+srv://hari:hE7NUnrzDbHQsncN@cluster0.ldtixwi.mongodb.net/';
+
+module.exports = {
+    mongoURI: dbPassword
+};
